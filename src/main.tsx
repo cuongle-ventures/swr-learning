6bf46962712ce71d8f80e95ca6fd5146eeecbb2f
Cuong Le <cuong.lm@cs.ventures>
2024-12-14 16:31:01 +0700

    config swr devtools

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routers.tsx';
 import ErrorBoundary from './components/ErrorBoundary.tsx';
 import FilterPostProvider from './components/FilterPostProvider.tsx';
+import { SWRDevTools } from 'swr-devtools';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <ErrorBoundary>
-            <SnackbarProvider>
-                <FilterPostProvider>
-                    <RouterProvider router={router} />
-                </FilterPostProvider>
-            </SnackbarProvider>
+            <SWRDevTools>
+                <SnackbarProvider>
+                    <FilterPostProvider>
+                        <RouterProvider router={router} />
+                    </FilterPostProvider>
+                </SnackbarProvider>
+            </SWRDevTools>
         </ErrorBoundary>
     </StrictMode>,
 );
