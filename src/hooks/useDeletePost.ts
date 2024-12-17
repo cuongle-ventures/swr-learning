e039c21a00831bac34f1ba787010fc71b89ae2d3
Cuong Le <cuong.lm@cs.ventures>
2024-12-17 16:30:48 +0700

    update code delete logic

--- src/hooks/useDeletePost.ts
+++ src/hooks/useDeletePost.ts
@@ -3,6 +3,7 @@ import { KeyedMutator } from 'swr';
 import instance from '../api/axios';
 import { useRevalidatePostList } from './useRevalidatePostList';
 import { GetPostsResponse } from './useGetPosts';
+import useSWRMutation from 'swr/mutation';
 
 interface Props {
     mutate?: KeyedMutator<GetPostsResponse | undefined>;
@@ -11,6 +12,29 @@ interface Props {
 const useDeletePost = ({ mutate }: Props) => {
     const { revalidateAll } = useRevalidatePostList();
     const { enqueueSnackbar } = useSnackbar();
+
+    const { trigger, ...rest } = useSWRMutation(
+        'delete-user',
+        (_key, { arg }: { arg: { id: string } }) =>
+            new Promise((resolve, _reject) => {
+                setTimeout(() => {
+                    const randomErr = Math.floor(Math.random() * 2);
+                    if (randomErr === 1 && !import.meta.env.TEST) {
+                        return _reject(new Error('Something wrong'));
+                    }
+                    instance.delete('/posts/' + arg.id).then((data) => {
+                        enqueueSnackbar({
+                            variant: 'success',
+                            message: 'Delete ' + data.data.title + ' successfully',
+                        });
+                        resolve(data as any);
+                    });
+                }, 500);
+            }),
+        {
+            throwOnError: true,
+        },
+    );
 
     const handleDeletePost = async (id: string) => {
         try {
@@ -26,32 +50,18 @@ const useDeletePost = ({ mutate }: Props) => {
                 revalidate: false,
             });
 
-            await new Promise((resolve, _reject) => {
-                setTimeout(() => {
-                    const randomErr = Math.floor(Math.random() * 2);
-                    if (randomErr === 1 && !import.meta.env.TEST) {
-                        return _reject(new Error('Something wrong'));
-                    }
-                    instance.delete('/posts/' + id).then((data) => {
-                        enqueueSnackbar({
-                            variant: 'success',
-                            message: 'Delete ' + data.data.title + ' successfully',
-                        });
-                        resolve(data as any);
-                    });
-                }, 500);
-            });
+            await trigger({ id });
         } catch (error) {
             enqueueSnackbar({
                 variant: 'error',
-                message: 'Fail to delete item',
+                message: 'Fail to delete item ' + id,
             });
         } finally {
             revalidateAll();
         }
     };
 
-    return { handleDeletePost };
+    return { handleDeletePost, ...rest };
 };
 
 export default useDeletePost;
